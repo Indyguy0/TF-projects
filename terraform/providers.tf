e149960710db1e275361ce0cfe2d08c5ffd0f364
Indyguy0 <97036181+Indyguy0@users.noreply.github.com>
2025-09-17 23:53:13 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,6 +11,6 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA5JMSUE6WTJIURW4Q"
-  secret_key = "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"
+  access_key = "AKIA5JMSUE6W526XAV6N"
+  secret_key = "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"
 }
